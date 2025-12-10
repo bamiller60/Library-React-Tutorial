bd7e8c568d9aca3dedc6a73d78909d4e34ec44db
Brad <bamiller6024@gmail.com>
2025-12-10 17:31:55 -0500

    Initial Commit

---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Rating from '../components/ui/Rating';
+
+const BookInfo = ({ books }) => {
+    return (
+        <div id="books__body">
+            <main id="books__main">
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <Link to="/books" className="book__link">
+                              <FontAwesomeIcon icon="arrow-left" />
+                            </Link>
+                            <Link to="/books" className="book__link"><h2 className='book__selected--title--top'>Books</h2></Link>
+                        </div>
+                        <div className="book__selected">
+                            <figure className="book__selected--figure">
+                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selcted--img" />
+                            </figure>
+                            <div className="book__selected--description">
+                                <h2 className='book__selected--title'>Crack the Coding Interview</h2>
+                                <Rating rating="4.5" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default BookInfo;

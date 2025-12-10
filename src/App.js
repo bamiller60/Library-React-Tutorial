cc8f02b7528bf60ad40b1586dd5d145ca746ed76
Brad <bamiller6024@gmail.com>
2025-12-10 18:42:48 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
@@ -12,9 +12,13 @@ function App() {
     <Router>
     <div className="App">
       <Nav />
+
+      <Routes>
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
       <Route path="/books/1" render={() => <BookInfo books={books} />} />
+      </Routes>
+      
       <Footer />
     </div>
     </Router>

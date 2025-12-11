220e9720c977d868484d6f4fa72f27449b882436
Brad <bamiller6024@gmail.com>
2025-12-10 19:13:25 -0500

    Third Commit

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ import BookInfo from "./pages/BookInfo";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav />
+      <div className="App">
+        <Nav />
 
-      <Routes>
-      <Route path="/" exact component={Home} />
-      <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books/1" render={() => <BookInfo books={books} />} />
-      </Routes>
-      
-      <Footer />
-    </div>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact render={() => <Books books={books} />} />
+          <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        </Routes>
+
+        <Footer />
+      </div>
     </Router>
   );
 }

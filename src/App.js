1a1d86d2727292b78f883afa4fce48137d32d6e1
Brad <bamiller6024@gmail.com>
2026-02-04 14:45:22 -0500

    BookInfo find method error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import React, { useState, useEffect } from 'react';
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
@@ -6,19 +7,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+function addToCart(book) {
+  setCart([...cart, book])
+}
+
+useEffect(() => {
+  console.log(cart)
+}, [cart])
+
+
   return (
     <Router>
       <div className="App">
         <Nav />
-
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/books" exact render={() => <Books books={books} />} />
-          <Route path="/books/1" render={() => <BookInfo books={books} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} />} />
+           <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
-
         <Footer />
       </div>
     </Router>

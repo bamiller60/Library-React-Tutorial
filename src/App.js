3a8b4f56291289c418ad746ebe9e4c467bdab302
Brad <bamiller6024@gmail.com>
2026-03-06 14:22:17 -0500

    mountedRef.current

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
@@ -12,24 +12,53 @@ import Cart from "./pages/Cart";
 function App() {
   const [cart, setCart] = useState([]);
 
-function addToCart(book) {
-  setCart([...cart, book])
-}
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
 
-useEffect(() => {
-  console.log(cart)
-}, [cart])
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => 
+        item.id === book.id ? { ...item, quantity: +quantity } : item
+      )
+    );
+  }
 
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity;
+    })
+    return counter;
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
-          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} />} />
-           <Route path="/cart" element={<Cart books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />
+            }
+          />
         </Routes>
         <Footer />
       </div>
